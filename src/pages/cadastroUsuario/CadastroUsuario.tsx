e96e4b886e1e63824d4f0bbcfaf1156497455b6e
Samantha Alexandra <lexalaska31@gmail.com>
2023-04-18 20:55:35 -0300

    :sparkles: Terminando a Integração do Back com a Página de Cadastro

--- src/pages/cadastroUsuario/CadastroUsuario.tsx
+++ src/pages/cadastroUsuario/CadastroUsuario.tsx
@@ -62,14 +62,22 @@ function CadastroUsuario() {
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid item xs={12} md={6} className='imagemCadastro'></Grid>
-            <Grid item xs={12} md={6}  alignItems='center'>
-                <Box paddingX={10}  paddingY={{ xs: 5 }}>
+            <Grid item xs={12} md={6} alignItems='center'>
+                <Box paddingX={10} paddingY={{ xs: 5 }}>
                     <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>Cadastrar</Typography>
+
                         <TextField value={user.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='nome' label='nome' variant='outlined' name='nome' margin='normal' fullWidth />
+
                         <TextField value={user.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+
                         <TextField value={user.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+
                         <TextField value={confirmarSenha} onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='confirmarSenha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
+
+                        <TextField id='foto' label='foto' variant='outlined' name='foto' margin='normal' fullWidth />
+                        <Typography className='subtitulo'>*Coloque apenas o link da foto nesse campo</Typography>
+
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/login' className='text-decorator-none'>
                                 <Button variant='contained' color='secondary' className='botaoCancelar'>
